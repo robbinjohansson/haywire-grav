d5b95d5a15616076570e9a1c908c52040c4c41b5
Robbin Johansson <robbinfellow@gmail.com>
2017-01-31 13:17:30 +0100

    Update haywire.php

--- haywire.php
+++ haywire.php
@@ -1,8 +1,9 @@
 <?php
- namespace Grav\Theme;
+namespace Grav\Theme;
 
- class Haywire
- {
-     // Some new methods, properties etc.
- }
-?> 
+use Grav\Common\Theme;
+
+class Haywire extends Theme
+{
+    // Access plugin events in this class
+}
