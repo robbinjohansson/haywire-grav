28fce37a152e6eac772fd47defe5f11c89e281b5
Robbin Johansson <robbinfellow@gmail.com>
2018-02-08 10:39:12 +0100

    Modify asset cache busting handler

--- haywire.php
+++ haywire.php
@@ -27,13 +27,12 @@ class Haywire extends Theme
 
     public function onTwigSiteVariables()
     {
-        $path = $this->grav['base_url'] . $this->config['theme']['assetPath'];
         $manifest = __DIR__ . '/dist/mix-manifest.json';
 
         if (file_exists($manifest)) {
             $assets = json_decode(file_get_contents($manifest), true);
-            $this->grav['assets']->addJs($path . $assets['/js/app.js'], ['group' => 'bottom']);
-            $this->grav['assets']->addCss($path . $assets['/css/app.css']);
+            $this->grav['assets']->addJs('theme://dist/' . $assets['/js/app.js'], ['group' => 'bottom']);
+            $this->grav['assets']->addCss('theme://dist/' . $assets['/css/app.css'],10);
         }
     }
 
